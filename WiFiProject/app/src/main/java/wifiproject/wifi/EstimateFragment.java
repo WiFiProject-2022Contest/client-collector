38e38fc98757bc5e235d8c8d4d0f183d23a31c88
Dongjun Lim <5836756+copyrig@users.noreply.github.com>
2022-06-16 16:50:21 +0900

    프래그먼트 자바 코드 기초 생성

--- WiFiProject/app/src/main/java/wifiproject/wifi/EstimateFragment.java
+++ WiFiProject/app/src/main/java/wifiproject/wifi/EstimateFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,64 +17,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EstimateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EstimateFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EstimateFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EstimateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EstimateFragment newInstance(String param1, String param2) {
-        EstimateFragment fragment = new EstimateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button buttonEstimate;
+    TextView textResultEstimate;
+    List<WiFiItem> allData;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_estimate, container, false);
+        buttonEstimate = rootview.findViewById(R.id.buttonEstimate);
+        textResultEstimate = rootview.findViewById(R.id.textResultEstimate);
 
         // DB 전체 다 받아오기
         RetrofitAPI retrofit_api = RetrofitClient.getRetrofitAPI();
         retrofit_api.getData(-1, -1).enqueue(new Callback<List<WiFiItem>>() {
             @Override
             public void onResponse(Call<List<WiFiItem>> call, Response<List<WiFiItem>> response) {
-                List<WiFiItem> all = response.body();
+                allData = response.body();
             }
 
             @Override
@@ -80,6 +42,14 @@ public class EstimateFragment extends Fragment {
 
             }
         });
-        return inflater.inflate(R.layout.fragment_estimate, container, false);
+
+        buttonEstimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return rootview;
     }
 }
