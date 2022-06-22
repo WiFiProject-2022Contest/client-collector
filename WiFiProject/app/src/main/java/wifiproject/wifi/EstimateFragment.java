449e1350d47e140849593cf48d1160103f6bc0df
Dongjun Lim <5836756+copyrig@users.noreply.github.com>
2022-06-22 10:40:17 +0900

    위치 추정 이유 Textview를 세로 스크롤 가능하도록 변경

--- WiFiProject/app/src/main/java/wifiproject/wifi/EstimateFragment.java
+++ WiFiProject/app/src/main/java/wifiproject/wifi/EstimateFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -73,6 +74,7 @@ public class EstimateFragment extends Fragment {
         textResultEstimateWiFi2G = rootView.findViewById(R.id.textResultEstimateWiFi2G);
         textResultEstimateWiFi5G = rootView.findViewById(R.id.textResultEstimateWiFi5G);
         textEstimateReason = rootView.findViewById(R.id.textEstimateReason);
+        textEstimateReason.setMovementMethod(new ScrollingMovementMethod());
 
         buttonUpdateAllDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
