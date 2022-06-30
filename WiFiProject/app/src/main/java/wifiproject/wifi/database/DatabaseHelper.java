eea101c932cac69f394a5674bbe63a692a878745
geniemo <kor98won@gmail.com>
2022-06-30 15:36:15 +0900

    [add] DatabaseHelper 동기화 기능 추가

--- WiFiProject/app/src/main/java/wifiproject/wifi/database/DatabaseHelper.java
+++ WiFiProject/app/src/main/java/wifiproject/wifi/database/DatabaseHelper.java
@@ -1,10 +1,12 @@
 package wifilocation.wifi.database;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -15,15 +17,20 @@ import java.util.List;
 
 import wifilocation.wifi.estimate.EstimatedResult;
 import wifilocation.wifi.model.WiFiItem;
+import wifilocation.wifi.serverconnection.RetrofitAPI;
+import wifilocation.wifi.serverconnection.RetrofitClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * example)
  * DatabaseHelper dbHelper = new DatabaseHelper(context);
  * dbHelper.insertIntoWiFiInfo(parameters);
  * List<WiFiItem> items = dbHelper.searchFromWiFiInfo(parameters);
- *
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
+    Context context;
     public static final String DBNAME = "wifilocation1.db";
     public static int VERSION = 1;
 
@@ -55,6 +62,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DBNAME, null, VERSION);
+        this.context = context;
     }
 
     @Override
@@ -71,8 +79,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 DATE + " integer, " +
                 UUID + " text, " +
                 BUILDING + " text, " +
-                METHOD + " text)");
-        
+                METHOD + " text, " +
+                "new integer default 0)");
+
         // fingerprint 테이블 생성
         sqLiteDatabase.execSQL("create table if not exists " + TABLE_FINGERPRINT + " (" +
                 "id integer PRIMARY KEY autoincrement, " +
@@ -87,7 +96,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 BUILDING + " text, " +
                 SSID + " text, " +
                 ALGORITHM_VERSION + " integer, " +
-                METHOD + " text)");
+                METHOD + " text, " +
+                "new integer default 0)");
     }
 
     @Override
@@ -113,11 +123,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (items.size() == 0) {
             return;
         }
-        long currentTimeMillis = System.currentTimeMillis();
         String sql = "insert into " + TABLE_WIFIINFO + String.format(" (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ", POS_X, POS_Y, SSID, BSSID, FREQUENCY, LEVEL, DATE, UUID, BUILDING, METHOD) + " values ";
         for (WiFiItem item : items) {
             sql += String.format("(%f, %f, '%s', '%s', %d, %d, %d, '%s', '%s', '%s'), ",
-                    item.getX(), item.getY(), item.getSSID(), item.getBSSID(), item.getFrequency(), item.getRSSI(), currentTimeMillis, item.getUuid(), item.getBuilding(), item.getMethod());
+                    item.getX(), item.getY(), item.getSSID(), item.getBSSID(), item.getFrequency(), item.getRSSI(), item.getDate(), item.getUuid(), item.getBuilding(), item.getMethod());
         }
         sql = sql.substring(0, sql.length() - 2); // 끝에 붙은 ,<공백> 제거
         SQLiteDatabase db = getWritableDatabase();
@@ -128,22 +137,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * wifiinfo 테이블로부터 데이터 조회
      *
      * @param building building이 일치하는 row들만 조회
-     * @param ssid ssid가 일치하는 row들만 조회
-     * @param x x - 1 < pos_x < x + 1 인 row들만 조회
-     * @param y y - 1 < pos_y < y + 1 인 row들만 조회, x와 y 모두 null 전달 시 위치에 제한 없음
-     * @param from from 잉후에 등록된 row들만 조회, null 전달 시 제한 없음
-     * @param to to 이전에 등록된 row들만 조회, null 전달 시 제한 없음
+     * @param ssid     ssid가 일치하는 row들만 조회
+     * @param x        x - 1 < pos_x < x + 1 인 row들만 조회
+     * @param y        y - 1 < pos_y < y + 1 인 row들만 조회, x와 y 모두 null 전달 시 위치에 제한 없음
+     * @param from     from 잉후에 등록된 row들만 조회, null 전달 시 제한 없음
+     * @param to       to 이전에 등록된 row들만 조회, null 전달 시 제한 없음
+     * @param _new     서버에는 없고 로컬 DB에만 있는 데이터를 조회하려면 null이 아닌 값 전달
      * @return sql문 실행 결과로 나온 row들을 List<WiFiItem> 으로 변환하여 반환
      */
-    public List<WiFiItem> searchFromWiFiInfo(String building, String ssid, Float x, Float y, String from, String to) {
-        String sql = "select " + String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s", POS_X, POS_Y, SSID, BSSID, LEVEL, FREQUENCY, UUID, BUILDING, METHOD) + " from " + TABLE_WIFIINFO;
+    @SuppressLint("Range")
+    public List<WiFiItem> searchFromWiFiInfo(String building, String ssid, Float x, Float y, String from, String to, Integer _new) {
+        String sql = "select " + String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s", POS_X, POS_Y, SSID, BSSID, LEVEL, FREQUENCY, UUID, BUILDING, METHOD, DATE) + " from " + TABLE_WIFIINFO;
+        List<String> conditions = new ArrayList<String>();
         // 빌딩 조건 추가
         if (building != null) {
-            sql += String.format(" where (%s = '%s')", BUILDING, building);
+            conditions.add(String.format(" (%s = '%s') ", BUILDING, building));
         }
         // SSID 조건 추가
         if (ssid != null) {
-            sql += String.format(" and (%s = '%s')", SSID, ssid);
+            conditions.add(String.format(" (%s = '%s') ", SSID, ssid));
         }
         // 날짜 조건 추가
         if (from != null || to != null) {
@@ -152,16 +164,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 long timestampFrom, timestampTo;
                 timestampFrom = sdf.parse(from == null ? "20020202" : from).getTime();
                 timestampTo = sdf.parse(to == null ? "20300303" : to).getTime();
-                sql += String.format(" and (%s between %d and %d)", DATE, timestampFrom, timestampTo);
+                conditions.add(String.format(" (%s between %d and %d) ", DATE, timestampFrom, timestampTo));
             } catch (ParseException e) {
                 e.printStackTrace();
             }
         }
         // 위치 조건 추가
         if (x != null && y != null) {
-            sql += String.format(" and ((%s between %f and %f) and (%s between %f and %f))", POS_X, x - 1, x + 1, POS_Y, y - 1, y + 1);
+            conditions.add(String.format(" ((%s between %f and %f) and (%s between %f and %f))", POS_X, x - 1, x + 1, POS_Y, y - 1, y + 1));
         }
-
+        // 로컬에만 있는 데이터 검색할건지 조건 추가
+        if (_new != null) {
+            conditions.add(" (new = 1) ");
+        }
+        if (conditions.size() != 0) {
+            sql += " where ";
+        }
+        for (int i = 0; i < conditions.size(); i++) {
+            sql += conditions.get(i);
+            if (i != conditions.size() - 1) {
+                sql += " and ";
+            }
+        }
 
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery(sql, null);
@@ -171,15 +195,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         int count = cursor.getCount();
         for (int i = 0; i < count; i++) {
             cursor.moveToNext();
-            result.add(new WiFiItem(cursor.getFloat(0),
-                    cursor.getFloat(1),
-                    cursor.getString(2),
-                    cursor.getString(3),
-                    cursor.getInt(4),
-                    cursor.getInt(5),
-                    cursor.getString(6),
-                    cursor.getString(7),
-                    cursor.getString(8)));
+            result.add(new WiFiItem(cursor.getFloat(cursor.getColumnIndex(POS_X)),
+                    cursor.getFloat(cursor.getColumnIndex(POS_Y)),
+                    cursor.getString(cursor.getColumnIndex(SSID)),
+                    cursor.getString(cursor.getColumnIndex(BSSID)),
+                    cursor.getInt(cursor.getColumnIndex(LEVEL)),
+                    cursor.getInt(cursor.getColumnIndex(FREQUENCY)),
+                    cursor.getString(cursor.getColumnIndex(UUID)),
+                    cursor.getString(cursor.getColumnIndex(BUILDING)),
+                    cursor.getString(cursor.getColumnIndex(METHOD)),
+                    cursor.getLong(cursor.getColumnIndex(DATE))));
         }
         return result;
     }
@@ -206,23 +231,126 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * fingerprint 테이블에 데이터 추가
-     * 
+     *
      * @param items 추정된 위치 정보 리스트 전달
      */
     public void insertIntoFingerprint(List<EstimatedResult> items) {
         if (items.size() == 0) {
             return;
         }
-        long currentTimeMillis = System.currentTimeMillis();
         String sql = "insert into " + TABLE_FINGERPRINT + String.format(" (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", POS_X, POS_Y, UUID, DATE, EST_X, EST_Y, K, THRESHOLD, BUILDING, SSID, ALGORITHM_VERSION, METHOD) + " values ";
         for (EstimatedResult item : items) {
             sql += String.format("(%f, %f, '%s', %d, %f, %f, %d, %d, '%s', '%s', %d, '%s'), ",
-                    item.getPositionRealX(), item.getPositionRealY(), item.getUuid(), currentTimeMillis, item.getPositionEstimatedX(), item.getPositionEstimatedY(),
+                    item.getPositionRealX(), item.getPositionRealY(), item.getUuid(), item.getDate(), item.getPositionEstimatedX(), item.getPositionEstimatedY(),
                     item.getK(), item.getThreshold(), item.getBuilding(), item.getSsid(), item.getAlgorithmVersion(), item.getMethod());
         }
         sql = sql.substring(0, sql.length() - 2);
 
         SQLiteDatabase db = getWritableDatabase();
         db.execSQL(sql);
+    }
+
+    /**
+     * fingerprint 테이블에서 데이터 조회
+     *
+     * @param _new 서버에는 없고 로컬 DB에만 있는 데이터를 조회하려면 null이 아닌 값 전달
+     * @return sql문 실행 결과로 나온 row들을 List<EstimateResult> 로 바꿔서 반환
+     */
+    @SuppressLint("Range")
+    public List<EstimatedResult> searchFromFingerprint(Integer _new) {
+        String sql = "select " + String.format(" %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s ", POS_X, POS_Y, UUID, DATE, EST_X, EST_Y, K, THRESHOLD, BUILDING, SSID, ALGORITHM_VERSION, METHOD) +
+                " from " + TABLE_FINGERPRINT;
+        List<String> conditions = new ArrayList<String>();
+        // 로컬에만 있는 데이터만 조회할지에 대한 조건 추가
+        if (_new != null) {
+            conditions.add(" (new = 1) ");
+        }
+        if (conditions.size() != 0) {
+            sql += " where ";
+        }
+        for (int i = 0; i < conditions.size(); i++) {
+            sql += conditions.get(i);
+            if (i != conditions.size() - 1) {
+                sql += " and ";
+            }
+        }
+
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<EstimatedResult> result = new ArrayList<EstimatedResult>();
+        int count = cursor.getCount();
+        for (int i = 0; i < count; i++) {
+            cursor.moveToNext();
+            result.add(new EstimatedResult(cursor.getString(cursor.getColumnIndex(BUILDING)),
+                    cursor.getString(cursor.getColumnIndex(SSID)),
+                    cursor.getFloat(cursor.getColumnIndex(POS_X)),
+                    cursor.getFloat(cursor.getColumnIndex(POS_Y)),
+                    cursor.getFloat(cursor.getColumnIndex(EST_X)),
+                    cursor.getFloat(cursor.getColumnIndex(EST_Y)),
+                    cursor.getString(cursor.getColumnIndex(UUID)),
+                    cursor.getString(cursor.getColumnIndex(METHOD)),
+                    cursor.getInt(cursor.getColumnIndex(K)),
+                    cursor.getInt(cursor.getColumnIndex(THRESHOLD)),
+                    cursor.getInt(cursor.getColumnIndex(ALGORITHM_VERSION)),
+                    cursor.getLong(cursor.getColumnIndex(DATE))));
+        }
+        return result;
+    }
+
+    /**
+     * 서버와 DB 동기화
+     */
+    public void synchronize() {
+        // 서버에 new가 1인 데이터 올리기
+        pushRemoteNewData();
+        // 로컬에 있는 데이터 삭제
+        deleteAllLocal();
+        // 서버로부터 데이터 받아오기
+        getAllFromRemote();
+        Toast.makeText(context, "동기화 종료", Toast.LENGTH_SHORT).show();
+    }
+
+    private void pushRemoteNewData() {
+        List<WiFiItem> wiFiItems = searchFromWiFiInfo(null, null, null, null, null, null, 1);
+        List<EstimatedResult> estimatedResults = searchFromFingerprint(1);
+        RetrofitAPI retrofitAPI = RetrofitClient.getRetrofitAPI();
+        retrofitAPI.postDataWiFiItem(wiFiItems);
+        retrofitAPI.postDataEstimatedResult(estimatedResults);
+    }
+
+    private void deleteAllLocal() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("delete from " + TABLE_WIFIINFO);
+        db.execSQL("delete from " + TABLE_FINGERPRINT);
+    }
+
+    private void getAllFromRemote() {
+        RetrofitAPI retrofitAPI = RetrofitClient.getRetrofitAPI();
+        retrofitAPI.getDataWiFiItem(null, null, null, null, null, null).enqueue(new Callback<List<WiFiItem>>() {
+            @Override
+            public void onResponse(Call<List<WiFiItem>> call, Response<List<WiFiItem>> response) {
+                List<WiFiItem> body = response.body();
+                insertIntoWiFiInfo(body);
+            }
+
+            @Override
+            public void onFailure(Call<List<WiFiItem>> call, Throwable t) {
+                Toast.makeText(context, "데이터 동기화 - 서버로부터 로드 실패 (WiFiItem)", Toast.LENGTH_SHORT).show();
+            }
+        });
+        retrofitAPI.getDataEstimateResult(null, null).enqueue(new Callback<List<EstimatedResult>>() {
+            @Override
+            public void onResponse(Call<List<EstimatedResult>> call, Response<List<EstimatedResult>> response) {
+                List<EstimatedResult> body = response.body();
+                insertIntoFingerprint(body);
+            }
+
+            @Override
+            public void onFailure(Call<List<EstimatedResult>> call, Throwable t) {
+                Toast.makeText(context, "데이터 동기화 - 서버로부터 로드 실패 (EstimatedResult)", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
