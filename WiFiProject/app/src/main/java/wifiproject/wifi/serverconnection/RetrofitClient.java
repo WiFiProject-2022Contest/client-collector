834f540191c3262da3ad8c8ee77df32f0f774a8a
geniemo <kor98won@gmail.com>
2022-07-06 17:10:01 +0900

    [edit] Date 형식 때문에 새 데이터 insert 안되는 문제 해결
    - Gson과 jackson 날짜 형식이 안맞아서 생긴 문제여서 RetrofitAPI에서 converter 만들 때 jackson에 맞게 gson 형식을 만들어서 넣어줬다.

--- WiFiProject/app/src/main/java/wifiproject/wifi/serverconnection/RetrofitClient.java
+++ WiFiProject/app/src/main/java/wifiproject/wifi/serverconnection/RetrofitClient.java
@@ -1,5 +1,8 @@
 package wifilocation.wifi.serverconnection;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,9 +12,12 @@ public class RetrofitClient {
 
     public static RetrofitAPI getRetrofitAPI() {
         if (retrofit_api == null) {
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                    .create();
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
             retrofit_api = retrofit.create(RetrofitAPI.class);
         }
